340f18e5e8e7ac630058f4be6c2235f055f7951b
Chen Wu <overwindows@gmail.com>
2025-10-14 16:02:57 +0000

    Update eval.sh to correct experiment name and requirements.txt to upgrade torch version

--- eval.sh
+++ eval.sh
@@ -21,7 +21,7 @@ export SAMBANOVA_API_KEY=042ca35c-beaf-4f5b-8033-9170556e5251
 # EXP_NAME=DeepSeek-V3.1
 
 MODEL_NAME=Qwen3-32B
-EXP_NAME=Qwen3-32B_1014
+EXP_NAME=Qwen3-32B
 
 # python evaluation.py --model_name $MODEL_NAME --proc_num 4 --vllm_url http://127.0.0.1:8000/v1 --exp_name $EXP_NAME
 python evaluation.py --model_name $MODEL_NAME --proc_num 1 --exp_name $EXP_NAME
